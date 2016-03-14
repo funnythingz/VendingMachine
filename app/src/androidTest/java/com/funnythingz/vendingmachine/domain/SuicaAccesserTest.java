34c2db04a9f6f89356422ceb66989e899e36535b
funnythingz <funnythingz69+github@gmail.com>
2016-03-14 18:58:21 +0900

    added TODO

---
+++ app/src/androidTest/java/com/funnythingz/vendingmachine/domain/SuicaAccesserTest.java
@@ -0,0 +1,34 @@
+package com.funnythingz.vendingmachine.domain;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class SuicaAccesserTest extends TestCase {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testReadBalance() throws Exception {
+        // TODO:
+    }
+
+    @Test
+    public void testUpdateBalance() throws Exception {
+        // TODO:
+    }
+}
---
+++ app/src/androidTest/java/com/funnythingz/vendingmachine/domain/SuicaTest.java
@@ -0,0 +1,44 @@
+package com.funnythingz.vendingmachine.domain;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class SuicaTest extends TestCase {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateBalance() throws Exception {
+        // TODO:
+    }
+
+    @Test
+    public void testGetBalance() throws Exception {
+        // TODO:
+    }
+
+    @Test
+    public void testEquals() throws Exception {
+        // TODO:
+    }
+
+    @Test
+    public void testGetId() throws Exception {
+        // TODO:
+    }
+}
