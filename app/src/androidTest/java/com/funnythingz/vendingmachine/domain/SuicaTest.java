c647a46c60ca75785bd07776c599aa64ef81661f
funnythingz <funnythingz69+github@gmail.com>
2016-03-15 11:35:18 +0900

    Fixed SuicaTest

--- app/src/androidTest/java/com/funnythingz/vendingmachine/domain/SuicaTest.java
+++ app/src/androidTest/java/com/funnythingz/vendingmachine/domain/SuicaTest.java
@@ -5,16 +5,22 @@ import android.support.test.runner.AndroidJUnit4;
 import junit.framework.TestCase;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+
 @RunWith(AndroidJUnit4.class)
 public class SuicaTest extends TestCase {
 
+    private Suica suica;
+
     @Before
     public void setUp() throws Exception {
-
+        suica = new Suica(new SuicaId("s1"), new Balance(1000));
     }
 
     @After
@@ -23,22 +29,24 @@ public class SuicaTest extends TestCase {
     }
 
     @Test
-    public void testUpdateBalance() throws Exception {
-        // TODO:
+    public void testGetId() throws Exception {
+        Assert.assertEquals(suica.getId().getValue(), "s1");
     }
 
     @Test
     public void testGetBalance() throws Exception {
-        // TODO:
+        Assert.assertThat(suica.getBalance(), is(instanceOf(Balance.class)));
     }
 
     @Test
     public void testEquals() throws Exception {
-        // TODO:
+        Suica that = new Suica(new SuicaId("s2"), new Balance(1000));
+        Assert.assertFalse(suica.equals(that));
     }
 
     @Test
-    public void testGetId() throws Exception {
-        // TODO:
+    public void testUpdateBalance() throws Exception {
+        suica.updateBalance(new Balance(1500));
+        Assert.assertEquals(suica.getBalance().getValue(), 1500);
     }
 }
