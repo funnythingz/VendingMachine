8c10e640b9202bcf234a1c6df6bc94dfb7bd7e2f
funnythingz <funnythingz69+github@gmail.com>
2016-03-15 12:02:15 +0900

    Fixed SuicaAccesserTest

--- app/src/androidTest/java/com/funnythingz/vendingmachine/domain/SuicaAccesserTest.java
+++ app/src/androidTest/java/com/funnythingz/vendingmachine/domain/SuicaAccesserTest.java
@@ -5,16 +5,22 @@ import android.support.test.runner.AndroidJUnit4;
 import junit.framework.TestCase;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
 @RunWith(AndroidJUnit4.class)
 public class SuicaAccesserTest extends TestCase {
 
+    private Suica suica;
+
     @Before
     public void setUp() throws Exception {
-
+        suica = new Suica(new SuicaId("s1"), new Balance(5000));
     }
 
     @After
@@ -24,11 +30,15 @@ public class SuicaAccesserTest extends TestCase {
 
     @Test
     public void testReadBalance() throws Exception {
-        // TODO:
+        Balance readedBalance = SuicaAccesser.readBalance(suica);
+        Assert.assertThat(readedBalance, is(equalTo(suica.getBalance())));
+        Assert.assertEquals(readedBalance.getValue(), suica.getBalance().getValue());
     }
 
     @Test
     public void testUpdateBalance() throws Exception {
-        // TODO:
+        SuicaAccesser.updateBalance(suica, new Balance(10000));
+        Assert.assertEquals(suica.getBalance().getValue(), 10000);
+        Assert.assertNotEquals(suica.getBalance().getValue(), 5000);
     }
 }
